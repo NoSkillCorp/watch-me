e9870af308647ee8b030763568658d89b9440639
Caillou <igorlacroute@gmail.com>
2016-04-02 12:57:03 +0000

    removes the route helper of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+  root 'static_pages#home'
+  #TODO add this devise route when the UserController will be up
+  #devise_for :users
+  
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
